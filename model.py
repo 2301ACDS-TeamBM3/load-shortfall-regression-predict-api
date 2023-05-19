6409c8e0158af003ddd192498c71a7addc9b4b63
Elrich-sam <rsam00103@gmail.com>
2023-05-19 13:04:42 +0000

    my data engimeering added here

--- model.py
+++ model.py
@@ -58,7 +58,75 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    #dropping Valencia_pressure
+    feature_vector_df = feature_vector_df.drop('Valencia_pressure', axis=1)
+
+    #converting time to datetime object
+    feature_vector_df['time'] = pd.to_datetime(feature_vector_df['time'])
+
+    #creating new features from the time feature
+    feature_vector_df['year'] = feature_vector_df['time'].dt.year.astype(int)#creating year column
+    feature_vector_df['month'] = feature_vector_df['time'].dt.month.astype(int)#creating month column
+    feature_vector_df['day'] = feature_vector_df['time'].dt.day.astype(int)#creating day column
+    feature_vector_df['hour'] = feature_vector_df['time'].dt.hour.astype(int)#creating hour column
+
+    #extracting numerical values from the feature "Seville_pressure" and converting to integer
+    feature_vector_df['Seville_pressure'] = feature_vector_df['Seville_pressure'].str.extract('(\d+)', expand=False)
+    feature_vector_df['Seville_pressure'] = feature_vector_df['Seville_pressure'].astype(int)
+
+    #extracting numerical values from the feature "Valencia_wind_deg" and converting to integer
+    feature_vector_df['Valencia_wind_deg'] = feature_vector_df['Valencia_wind_deg'].str.extract('(\d+)', expand=False)
+    feature_vector_df['Valencia_wind_deg'] = feature_vector_df['Valencia_wind_deg'].astype(int)
+
+    #adding a new feature "Season" 
+    month_day = feature_vector_df['time'].dt.strftime('%m' '%d')
+
+    conditions = [
+    (month_day >= '0301') & (month_day <= '0531'),
+    (month_day >= '0601') & (month_day <= '0831'),
+    (month_day >= '0901') & (month_day <= '1130'),
+    (month_day >= '1201') & (month_day <= '1231') & (month_day >= '0101') & (month_day <= '0229')
+    ]
+
+    # 0 = spring, 1 = summer, 2 = autumn, 3 = winter
+    season = [0, 1, 2, 3]
+
+    # create a new column and use np.select to assign values to it using our lists as arguments
+    feature_vector_df['Season'] = np.select(conditions, season)
+
+    #adding new feature "Duration"
+    hour = feature_vector_df['time'].dt.strftime('%H')
+
+    conditions = [
+    (hour >= '06') & (hour <= '11'),
+    (hour >= '12') & (hour <= '13'),
+    (hour >= '14') & (hour <= '17'),
+    (hour >= '18') & (hour <= '20'),
+    (hour >= '21') & (hour <= '24') & (hour >= '01') & (hour <= '05')
+    ]
+
+    # 0 = morning, 1 = noon, 2 = afternoon, 3 = evening, 4 = night
+    duration = [0, 1, 2, 3, 4]
+
+    # create a new column and use np.select to assign values to it using our lists as arguments
+    feature_vector_df['Duration'] = np.select(conditions, duration)
+
+    #dropping the 'time' and 'Unnamed: 0' columns
+    feature_vector_df = feature_vector_df.drop(['time','Unnamed: 0'], axis=1)
+
+    #dealing with outliers
+    for column in feature_vector_df.columns:
+        if feature_vector_df[column].kurt() > 5:
+            if feature_vector_df[column].min() == 0:
+                feature_vector_df[column] = feature_vector_df[column] + 1
+            feature_vector_df[column] = np.log(feature_vector_df[column])
+
+    #standardizing
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    feature_vector_df = scaler.fit_transform(feature_vector_df)
+
+    predict_vector = feature_vector_df
     # ------------------------------------------------------------------------
 
     return predict_vector
